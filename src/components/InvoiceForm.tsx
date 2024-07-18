b59a1135534c5a13060f8a8e489263d17e4d10b9
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-07-18 10:53:01 +0530

    Bill To Form

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -32,19 +32,72 @@ function InvoiceForm() {
       quantity: 1,
     },
   ]);
+
+  const onChange = (event) => {
+    setState((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
     <Form>
       <Row>
         <Col md={8} lg={9}>
-          <Card className="p-4 p-xl-5 my-3 my-xl-4">
-            <div className="d-flex flex-row align-items-start justify-content-between mb-3">
-              <div className="mb-2">
-                <span className="fw-bold">Current&nbsp; Date:&nbsp </span>
-                <span className="current-date">
-                  {new Date().toLocaleDateString()}
-                </span>
+          <Card className="d-flex p-4 p-xl-5 my-3 my-xl-4">
+            <div className="d-dlex flex-row justify-content-between">
+              <div className="d-flex flex-row mb-3">
+                <div className="mb-2">
+                  <span className="fw-bold">Current&nbsp; Date:&nbsp; </span>
+                  <span className="current-date">
+                    {new Date().toLocaleDateString()}
+                  </span>
+                </div>
+              </div>
+              <div className="d-flex flex-row mb-3">
+                <div className="mb-2">
+                  <span className="fw-bold">Invoice&nbsp; Number:&nbsp; </span>
+                  <span className="current-date">{state.invoiceNumber}</span>
+                </div>
               </div>
             </div>
+            <hr className="my-4" />
+            <Row className="mb-5">
+              <Col>
+                <Form.Label className="fw-bold">Customer Name:</Form.Label>
+                <Form.Control
+                  placeholder="Enter Name"
+                  value={state.billTo}
+                  type="text"
+                  name="billTo"
+                  className="my-2"
+                  onChange={onChange}
+                  autoComplete="name"
+                  required={true}
+                />
+                <Form.Label className="fw-bold">Customer Email:</Form.Label>
+                <Form.Control
+                  placeholder="Enter Email"
+                  value={state.billToEmail}
+                  type="email"
+                  name="billToEmail"
+                  className="my-2"
+                  onChange={onChange}
+                  autoComplete="email"
+                />
+                <Form.Label className="fw-bold">Customer Address:</Form.Label>
+                <Form.Control
+                  placeholder="Enter Address"
+                  value={state.billToAddress}
+                  type="text"
+                  name="billToAddress"
+                  className="my-2"
+                  onChange={onChange}
+                  autoComplete="address"
+                  required={true}
+                />
+              </Col>
+            </Row>
           </Card>
         </Col>
       </Row>
